4c7627ab460700789294441f5a9c0c07567c99a7
Radli1380 <ildarshakirow@mail.ru>
2024-05-13 20:25:47 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+# my_string = (input('Введите ваше имя' ))
+# print(my_string)
+print('my_string'.upper())
+print('my_string'.lower())
+print('my_string'.replace('_', ''))
+print('my_string'.find('m'))
+print('my_string'.find('g'))
+
